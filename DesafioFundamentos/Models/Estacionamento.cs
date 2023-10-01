12ba6f399d16a67588ca3b15a8761dc440e68970
Juliana Chaves Palm <105652602+julianachavespalm@users.noreply.github.com>
2023-10-01 14:31:40 -0300

    Melhorias nas Funcionalidades do Estacionamento
    Neste commit, realizamos várias melhorias nas funcionalidades do sistema de estacionamento:
    
    1. Validação de Placas: Implementamos uma validação para evitar que placas em branco ou duplicadas sejam adicionadas ao estacionamento. Agora, apenas placas válidas podem ser registradas.
    
    2. Funcionalidade de Remoção: Adicionamos a funcionalidade de remoção de veículos do estacionamento. Isso permite que os usuários retirem veículos registrados anteriormente.
    
    3. Cálculo do Valor Total: Adicionamos um cálculo para determinar o valor total com base na quantidade de horas que um veículo permaneceu estacionado. O cálculo é realizado como "precoInicial + (precoPorHora * horas)" e o valor total é exibido no console.
    
    4. Correção de Bugs: Corrigimos bugs que anteriormente impediam a remoção efetiva de veículos. Agora, a remoção funciona corretamente.
    
    5. Listagem de Veículos: Para melhorar a usabilidade, implementamos um loop utilizando `foreach` na função de listagem (`ListarVeiculos`). Isso permite listar todos os veículos registrados de forma clara no console.
    
    Essas alterações têm como objetivo aprimorar a usabilidade e a funcionalidade geral do sistema de estacionamento, tornando-o mais robusto e eficiente para os usuários.

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,32 +14,61 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            // Implementação: Solicitação de nomenclatura da placa
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string novaPlaca = Console.ReadLine();
+
+            // Implementação: adicionar novaPlaca à lista "veiculos" 
+            // se não for valor em branco ou duplicado.
+            if (!string.IsNullOrWhiteSpace(novaPlaca) && !veiculos.Contains(novaPlaca))
+            {
+                // Adiciona a nova placa à lista "veiculos"
+                veiculos.Add(novaPlaca);
+                Console.WriteLine($"A nova placa {novaPlaca} foi adicionada com sucesso!");
+            }
+            else
+            {
+                // Não adiciona a nova placa à lista "veiculos"
+                Console.WriteLine($"A placa {novaPlaca} não foi adicionada.");
+                Console.WriteLine("Valor em branco ou duplicado.");
+            }
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            // Implementação: armazena em "placa" o valor digitado pelo usuário
+            string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                decimal valorTotal = 0;
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                while (true)
+                {
+                    Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                    // Implementação: realiza a validação do valor digitado para quantidade de horas
+                    string validacaoHoras = Console.ReadLine();
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                    // Declara que horas é um decimal se não for um valor em branco ou nulo
+                    decimal horas;
+                    if (string.IsNullOrWhiteSpace(validacaoHoras) || !decimal.TryParse(validacaoHoras, out horas))
+                    {
+                        Console.WriteLine($"Você inseriu o valor {validacaoHoras}.");
+                        Console.WriteLine("Por favor, insira um valor válido para horas.");
+                    }
+                    // Se o valor de horas for válido, calcula o valor total
+                    else
+                    {
+                        valorTotal = precoInicial + (horas * precoPorHora);
+                        break;
+                    }
+                }
+
+                // Remove a placa da lista "veiculos"
+                veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -55,8 +84,11 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                // Implementação: laço de repetição foreach para exibir a lista de veículos
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
             }
             else
             {
